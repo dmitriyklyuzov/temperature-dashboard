bb20a433d2384d7590531dc933fe8f004cee0d8f
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2019-10-11 08:43:08 -0400

    set useFindAndModify option to false to prevent Mongo deprecation warning

--- config/db.js
+++ config/db.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 // connect to db
 const connectDb = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
 
-        console.log('MongoDb Connected');
-    }
-    catch (e) {
-        console.error(e.message);
-        process.exit(1); // exit with a failure
-    }
+    console.log("MongoDb Connected");
+  } catch (e) {
+    console.error(e.message);
+    process.exit(1); // exit with a failure
+  }
 };
 
 // export connectDb so we can use it in index
